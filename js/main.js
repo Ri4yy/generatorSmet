27f1c25fd89fa089dece3fb87f80a178fa2299a1
Ri4yy <maksimov.1913@list.ru>
2024-06-12 15:28:38 +0300

    upd

--- js/main.js
+++ js/main.js
@@ -11,4 +11,25 @@ document.addEventListener('DOMContentLoaded', () => {
         menu.classList.remove('open')
         document.querySelector('body').classList.remove('no-scroll')
     })
+
+    let mobileLink = document.querySelectorAll('.mobile-menu-list__link')
+
+    mobileLink.forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('open')
+            document.querySelector('body').classList.remove('no-scroll')
+        })
+    })
+    
+
+    let accordionItem = document.querySelectorAll('.accordion__item'),
+        accordionBtn = document.querySelectorAll('.accordion__btn')
+
+        accordionBtn.forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                btn.closest('.accordion__item').classList.toggle('open')
+                
+            })
+        })
+        
 })
